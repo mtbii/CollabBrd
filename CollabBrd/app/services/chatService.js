0290906385bdef3d252e40c1f6febf7f31011fbd
mbrunson94 <mbrunson94@gmail.com>
2014-10-13 22:42:49 -0400

    Fix chatService.js missing

---
+++ CollabBrd/app/services/chatService.js
@@ -0,0 +1,96 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('chatService', chatService);
+
+    chatService.$inject = ['Hub', '$http'];
+
+    function chatService(Hub, $http) {
+
+        var hub = new Hub('chatHub', {
+
+            //client side methods
+            listeners: {
+                'addNewMessageToPage': function (name, message) {
+                    if (service.receive) {
+                        service.receive(name, message);
+                    }
+                }
+            },
+
+            //server side methods
+            methods: ['send', 'joinRoom', 'leaveRoom'],
+
+            //query params sent on initial connection
+            queryParams: {
+                'token': 'exampletoken'
+            },
+
+            //handle connection error
+            errorHandler: function (error) {
+                console.error(error);
+            }
+
+        });
+
+        function receive() { }
+
+        function send(name, message, groupName) {
+            if (groupName == '/') {
+                groupName = 'World';
+            }
+            try {
+                // Call the Send method on the hub. 
+                hub.send(name, message, groupName);
+            }
+            catch (e) {
+                reconnect(send, [name, message, groupName]);
+            }
+        }
+
+
+        function join(groupName) {
+            if (groupName == '/') {
+                groupName = 'World';
+            }
+            try {
+                hub.joinRoom(groupName);
+            }
+            catch (e) {
+                reconnect(join, [groupName]);
+            }
+        }
+
+        function leave(groupName) {
+            if (groupName == '/') {
+                groupName = 'World';
+            }
+            try {
+                hub.leaveRoom(groupName);
+            }
+            catch (e) {
+                reconnect(leave, [groupName]);
+            }
+        }
+
+        function reconnect(callback, args) {
+            hub.connection.start().then(function () {
+                if (callback != null) {
+                    callback.apply(this, args);
+                }
+            });
+        }
+
+        var service = {
+            send: send,
+            receive: receive,
+            join: join,
+            reconnect: reconnect,
+            leave: leave
+        }
+
+        return service;
+    }
+})();
